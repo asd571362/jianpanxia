bb24b5f09d4dcf72c92282a25ace323fdb2210b0
Pan <317759206@qq.com>
2017-08-28 21:21:27 +0800

    完成首页制作

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 //主页js
+
+//点击显示目录
+//需解决问题：点击空白区域不显示导航功能  *@600 设置了nav:display none;
+function showList() {
+	var nav=document.getElementById('nav');
+	var show=nav.style.display;
+	if(show=="block"){
+		nav.style.display="none";
+	}else{
+		nav.style.display="block";
+	}
+}
+
